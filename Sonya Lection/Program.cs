6fe0cc7824d87ce3a06aa499de4fc160a57329b4
Dolthin <mertviyanarhistsuchara@gmail.com>
2020-06-12 11:05:00 +0300

    god's sackes

--- Sonya Lection/Program.cs
+++ Sonya Lection/Program.cs
@@ -8,22 +8,41 @@ namespace Sonya_Lection
 		{
 			Console.WriteLine("Press any key to start");
 			Console.ReadLine();
-			char[] k = new[] {'H', 'e', 'l', 'l', 'o'};
-			Loca(k);
-
-
+			int[] j = m();
+			int[] u = m();
+			var sum = ArrayPlusArray(j, u);
+			Console.WriteLine(sum);
 			Console.WriteLine("Press any key to end");
 			Console.ReadLine();
 		}
-
-		public static void Loca(char[] Cool)
+		public static int ArrayPlusArray(int[] arr1, int[] arr2)
 		{
-			int i = 0;
-			while (i < Cool.Length)
+			int n = 0;
+			foreach (var i in arr1)
 			{
-				Console.WriteLine($"{Cool[i]}");
-				i++;
+				n += i;
 			}
+
+			int y = 0;
+			foreach (var g in arr2)
+			{
+				y += g;
+			}
+			return n+y;
+		}
+
+		public static int[] m()
+		{
+			Console.WriteLine("Напиши количество");
+			int c = Convert.ToInt32(Console.ReadLine());
+			int[] Rubbish = new int[c];
+			for (int i = 0; i < c; i++)
+			{
+				Console.WriteLine("Write a number");
+				Rubbish[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			return Rubbish;
 		}
 	}
 }

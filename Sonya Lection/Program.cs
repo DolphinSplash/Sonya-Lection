75b02fbe618173958ca81e197853c90d7ae55102
Dolthin <mertviyanarhistsuchara@gmail.com>
2020-06-12 10:11:58 +0300

    Add project files.

---
+++ Sonya Lection/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Sonya_Lection
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Press any key to start");
+			Console.ReadLine();
+			char[] k = new[] {'H', 'e', 'l', 'l', 'o'};
+			Loca(k);
+			Console.WriteLine("Press any key to end");
+			Console.ReadLine();
+		}
+
+		public static void Loca(char[] Cool)
+		{
+			int i = 0;
+			while (i < Cool.Length)
+			{
+				Console.WriteLine($"{Cool[i]}");
+				i++;
+			}
+		}
+	}
+}
